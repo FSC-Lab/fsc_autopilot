f9b65d9efd666a1ef333ef6bfb04a2695b751843
H S Helson Go <hellston20a@gmail.com>
2024-08-27 17:02:09 -0400

    Add angle function tests
    Adjust Isclose implementation to comply with python

--- fsc_autopilot/tests/src/test_isclose.cpp
+++ fsc_autopilot/tests/src/test_isclose.cpp
@@ -1,20 +1,23 @@
-
-/**
- * Test suite to demonstrate a good method for comparing floating-point values
- * using an epsilon. Run via JUnit 4.
- *
- * Note: this function attempts a "one size fits all" solution. There may be
- * some edge cases for which it still produces unexpected results, and some of
- * the tests it was developed to pass probably specify behaviour that is not
- * appropriate for some applications, especially concerning very small values
- * with differing signs.
- *
- * Before using it, make sure it's appropriate for your application!
- *
- * From http://floating-point-gui.de
- *
- * @author Michael Borgwardt
- */
+// Copyright © 2024 FSC Lab
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 #include <limits>
 
